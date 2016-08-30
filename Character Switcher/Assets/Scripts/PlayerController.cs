2fd68acd38e5015a4eb3ec05ade61cafd798bbdf
J-Dodds <1004254@student.sae.edu.au>
2016-08-30 14:27:11 +1000

    Player currently being controlled can move forward, backward, left and right. Currently, there is no intention to add camera rotation

--- Character Switcher/Assets/Scripts/PlayerController.cs
+++ Character Switcher/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float moveForce = 10.0f;
+
+    private Rigidbody rigBod;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        rigBod = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+    {
+	    if (Input.GetKey(KeyCode.W))
+        {
+            rigBod.AddForce(Vector3.forward * moveForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigBod.AddForce(Vector3.back * moveForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigBod.AddForce(Vector3.left * moveForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigBod.AddForce(Vector3.right * moveForce * Time.deltaTime);
+        }
 	}
 }
