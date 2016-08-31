af017641f8ac7573b23e34cde9d18368b6a66ca3
Jordan Liam Dodds <jdodds@sae.au>
2016-08-31 15:09:39 +1000

    Refactored character switch, now supports unlimited number of character objects Bug: Currently only controls the first character

--- Character Switcher/Assets/Scripts/CharacterSwitcher.cs
+++ Character Switcher/Assets/Scripts/CharacterSwitcher.cs
@@ -4,125 +4,63 @@ using System.Collections.Generic;
 
 public class CharacterSwitcher : MonoBehaviour
 {
+    [System.Serializable]
+    public class CharacterToggleData
+    {
+        public List<MonoBehaviour> characterScripts;
+        public GameObject rootObject, cameraAttachPoint;
+
+        //Activates scripts attached to current character
+        public void SwitchTo(bool state)
+        {
+            for (int i = 0; i < characterScripts.Count; i++)
+            {
+                characterScripts[i].enabled = state;
+            }
+        }
+    }
+    
     //The main camera in the game
     public GameObject mainCamera;
-    
-    //The list for each script on a character that you want to activate/deactivate
-    public List<MonoBehaviour> characterOneScripts;
-    public List<MonoBehaviour> characterTwoScripts;
-    public List<MonoBehaviour> characterThreeScripts;
 
-    //State machine representing every potential character that will be switched to
-    public enum CurrentCharacter
-    {
-        eccCharacterOne,
-        eccCharacterTwo,
-        eccCharacterThree,
-    }
-    private CurrentCharacter currentCharacter = 0;
+    //List of sets of character information from the above CharacetrToggleData class
+    public List<CharacterToggleData> characterDatas;
+
+    //Index of currently selected character
+    public int selectedCharacterIndex;
 
     void Update()
     {
-        SwitchUp();
-        SwitchDown();
-        ActivatingScripts();
-
-        //Sets camera position to a pre designated position and rotation using a tagged object
-        mainCamera.transform.position = GameObject.FindGameObjectWithTag("Character" + ((int)currentCharacter + 1) + "Camera").transform.position;
-        mainCamera.transform.rotation = GameObject.FindGameObjectWithTag("Character" + ((int)currentCharacter + 1) + "Camera").transform.rotation;
-
-    }
-
-    //On pressing E, character will switch in an ascending order, or resetting to the first character if the last character is active
-    void SwitchUp()
-    {                                                          //Will need to be changed to the LAST character you want to switch to
-        if (Input.GetKeyDown(KeyCode.E) && currentCharacter != CurrentCharacter.eccCharacterThree)
+        //On input 1, handle switching up the index of CharacterDatas
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            currentCharacter += 1;
-            Debug.Log("Player " + currentCharacter);
-        }                                                           //Will need to be changed to the LAST character you want to switch to
-        else if (Input.GetKeyDown(KeyCode.E) && currentCharacter == CurrentCharacter.eccCharacterThree)
-        {
-                               //Will need to be chnaged to the FIRST character you want to switch to
-            currentCharacter = CurrentCharacter.eccCharacterOne;
-            Debug.Log("Player " + currentCharacter);
+            HandleSwitching(1);
         }
+        //On input 2, handle switching down the index of CharacterDatas
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            HandleSwitching(-1);
+        }
+
+        //Moves camera to the camera attach point of the currently selected character
+        mainCamera.transform.position = characterDatas[selectedCharacterIndex].cameraAttachPoint.transform.position;
+        mainCamera.transform.rotation = characterDatas[selectedCharacterIndex].cameraAttachPoint.transform.rotation;
+
     }
 
-    //On pressing Q, character will switch in decending order, or resetting to the last character if the first character is active
-    void SwitchDown()
+    //Deactivates current object, moves by the offset in the array, and activates the new current object. Wraps arounds the ends of the array
+    void HandleSwitching(int offset)
     {
-                                                               //Will need to be chnaged to the FIRST character you want to switch to
-        if (Input.GetKeyDown(KeyCode.Q) && currentCharacter != CurrentCharacter.eccCharacterOne)
-        {
-            currentCharacter -= 1;
-            Debug.Log("Player " + currentCharacter);
-        }
-                                                                    //Will need to be chnaged to the FIRST character you want to switch to
-        else if (Input.GetKeyDown(KeyCode.Q) && currentCharacter == CurrentCharacter.eccCharacterOne)
-        {
-                               //Will need to be changed to the LAST character you want to switch to
-            currentCharacter = CurrentCharacter.eccCharacterThree;
-            Debug.Log("Player " + currentCharacter);
-        }
+        SetActiveCurrentCharacter(false);
+        selectedCharacterIndex += offset;
+        selectedCharacterIndex += characterDatas.Count;
+        selectedCharacterIndex %= characterDatas.Count;
+        SetActiveCurrentCharacter(true);
     }
 
-    //When the character is switched, handles activatibg and deactivating any scripts that are in the list of scripts. A new for each will need to be created for each new list of scripts
-    void ActivatingScripts()
+    //Handles the activation/deactivation based on the state bool
+    void SetActiveCurrentCharacter(bool state)
     {
-        //If the third character is the current character, activate the character 3 scripts and deactivate the others
-        if (currentCharacter == CurrentCharacter.eccCharacterThree)
-        {
-            foreach (MonoBehaviour script in characterThreeScripts)
-            {
-                script.enabled = true;
-            }
-
-            foreach (MonoBehaviour script in characterTwoScripts)
-            {
-                script.enabled = false;
-            }
-
-            foreach (MonoBehaviour script in characterOneScripts)
-            {
-                script.enabled = false;
-            }
-        }
-        //If the second character is the current character, activate the character 2 scripts and deactivate the others
-        else if (currentCharacter == CurrentCharacter.eccCharacterTwo)
-        {
-            foreach (MonoBehaviour script in characterThreeScripts)
-            {
-                script.enabled = false;
-            }
-
-            foreach (MonoBehaviour script in characterTwoScripts)
-            {
-                script.enabled = true;
-            }
-
-            foreach (MonoBehaviour script in characterOneScripts)
-            {
-                script.enabled = false;
-            }
-        }
-        //If the one character is the current character, activate the character 1 scripts and deactivate the others
-        else if (currentCharacter == CurrentCharacter.eccCharacterOne)
-        {
-            foreach (MonoBehaviour script in characterThreeScripts)
-            {
-                script.enabled = false;
-            }
-
-            foreach (MonoBehaviour script in characterTwoScripts)
-            {
-                script.enabled = false;
-            }
-
-            foreach (MonoBehaviour script in characterOneScripts)
-            {
-                script.enabled = true;
-            }
-        }
-    }
+        characterDatas[selectedCharacterIndex].SwitchTo(state);
+    }  
 }
